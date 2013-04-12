bf3879f4744799a55b650a37aceaa5d54ecfe0c0
ajit-kumar-azad <ajit.kumar@walkingtree.in>
2013-04-12 20:45:27 +0530

    Added code to demonstrate the usage of touches, targetTouches, and changedTouches properties of the touch event object

--- ch09/ch09_01.js
+++ ch09/ch09_01.js
@@ -4,8 +4,30 @@ Ext.application({
 	launch: function() {
     
     this.handleEvent = function(e, t, opts, ec) {
-	    	if (!Ext.isEmpty(ec.info))
-	    		console.log(ec.info.eventName);
+	    	if (!Ext.isEmpty(ec.info)) {
+	    		var en = ec.info.eventName;
+	    		console.log(en);
+	    		
+	    		if (en == 'touchstart') {
+	    			if (e.touches[0] == e.targetTouches[0]) {
+	    				console.log('Container covers the complete touchable area!');
+	    			}
+	    			
+	    			if (e.touches.length == e.targetTouches.length) {
+	    				console.log('All points are on target element');
+	    			}
+	    			
+	    			if (e.touches.length > 1) {
+	    				console.log('Device supports multiple touch points!');
+	    			}
+	    		}
+	    		
+	    		if (en === 'touchend') {
+	    			console.log('Removed: ' + e.changedTouches.length);
+	    			console.log('Remaining on element: ' + e.targetTouches.length);
+	    			console.log('Remaining on document: ' + e.touches.length);
+	    		}
+	    	}
     }
     
     this.handleScrollEvent = function(e) {
@@ -23,14 +45,14 @@ Ext.application({
 	var pnl = Ext.create('Ext.Container', {
 		id:'main-panel',
 	    fullscreen: true,
-	    scrollable: 'vertical',
+//	    scrollable: 'vertical',
 	    	layout: 'fit'
 	});
 
 	var touchPnl = Ext.getCmp('main-panel');
-	touchPnl.getScrollable().getScroller().on('scrollstart',this.handleScrollstartEvent);
-	touchPnl.getScrollable().getScroller().on('scroll',this.handleScrollEvent);
-	touchPnl.getScrollable().getScroller().on('scrollend',this.handleScrollendEvent);
+//	touchPnl.getScrollable().getScroller().on('scrollstart',this.handleScrollstartEvent);
+//	touchPnl.getScrollable().getScroller().on('scroll',this.handleScrollEvent);
+//	touchPnl.getScrollable().getScroller().on('scrollend',this.handleScrollendEvent);
 	Ext.get('main-panel').on({
             touchstart: this.handleEvent,
             touchend: this.handleEvent,
